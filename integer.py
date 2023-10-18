c7c7f9cb6e75ade025f1a774e274dd6a54e84f51
Lily Carroll <lily.carroll@stu.ocsb.ca>
2023-10-18 20:57:11 +0000

    finished integer.py

---
+++ integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by: Lily Carroll
+# Created on: Oct/18/2023
+# This program allows for the user to input an integer and my program will output if it is positive, negative or 0.
+
+def main():
+    # Declaring variable and getting the user input.
+    user_number = int(input("Enter an integer: "))
+
+    # Using If condition for positive integers.
+    if (user_number > 0):
+        print("Your number is positive.")
+
+    # Using Elif condition for negative integers.
+    elif (user_number < 0):
+        print("Your number is negative.")
+
+    # Using Else condition for the integer 0.
+    else:
+        print("Your number is 0.")
+
+
+if __name__ == "__main__":
+    main()
